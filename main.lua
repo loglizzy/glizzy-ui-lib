6a1560e19ed3039930b5a21be30c86080478a83e
loglizzy <72479668+loglizzy@users.noreply.github.com>
2021-03-03 22:59:50 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -34,6 +34,46 @@ function library:Window(title)
     Frame.Position = UDim2.new(0.383306324, 0, 0.138795987, 0)
     Frame.Size = UDim2.new(0, 157, 0, 42)
     
+    local UserInputService = game:GetService("UserInputService")
+
+    local gui = Frame
+    
+    local dragging
+    local dragInput
+    local dragStart
+    local startPos
+    
+    local function update(input)
+    	local delta = input.Position - dragStart
+    	gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+    end
+    
+    gui.InputBegan:Connect(function(input)
+    	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+    		dragging = true
+    		dragStart = input.Position
+    		startPos = gui.Position
+    		
+    		input.Changed:Connect(function()
+    			if input.UserInputState == Enum.UserInputState.End then
+    				dragging = false
+    			end
+    		end)
+    	end
+    end)
+    
+    gui.InputChanged:Connect(function(input)
+    	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+    		dragInput = input
+    	end
+    end)
+    
+    UserInputService.InputChanged:Connect(function(input)
+    	if input == dragInput and dragging then
+    		update(input)
+    	end
+    end)
+    
     _title.Name = "title"
     _title.Parent = Frame
     _title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
